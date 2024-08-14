661d2d60db177266681dad0586b86687ce3591bd
kumarna <naveenkudithi@Naveens-MacBook-Pro.local>
2024-08-14 16:34:33 +0530

    clousers

--- gobyexample/functions/main.go
+++ gobyexample/functions/main.go
@@ -14,8 +14,30 @@ func multiply(a, b int) int {
 
 }
 
+// function with multiple return values
+
+func mul(a, b int) (int, int, int){
+
+	return a, b, a*b
+
+}
+
+// variadac functions
+
+func sum(nums ...int) {
+	fmt.Print(nums, " ")
+	total := 0
+		for _, v := range(nums) {
+			total += v
+		}
+	fmt.Println(total)
+
+}
 func main() {
 
 	add(5,9)
+
 	fmt.Println(multiply(5,9))
+	fmt.Println(mul(5,9))
+	sum(1, 2, 3, 4)
 }
