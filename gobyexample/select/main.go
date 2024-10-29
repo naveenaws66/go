1f83918e833bce3320227d9e23165a76053843fb
naveenaws66 <naveenkudithi@Naveens-MacBook-Pro.local>
2024-10-29 11:28:50 +0530

    selct

---
+++ gobyexample/select/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		c1 <- "one"
+	}()
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "two"
+	}()
+
+	for i := 0; 
