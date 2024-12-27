50e56ee0acda53a4dd393a94b37f5e5d05a84c8b
naveenaws66 <naveenkudithi@Naveens-MacBook-Pro.local>
2024-12-27 14:52:47 +0530

    added time

---
+++ gobyexample/time/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	p := fmt.Println
+
+	now := time.Now()
+	p(now)
+
+	then := time.Date(
+		2009, 11, 17, 20, 34, 58, 651387237, time.UTC)
+
+	p(then)
+
+	p(then.Year())
+	p(then.Month())
+
+}
