5dc808e999576e37d4e6f521846bdde77bf5a7d9
naveenaws66 <naveenkudithi@Naveens-MacBook-Pro.local>
2025-01-14 15:11:48 +0530

    read csv file and print contents

--- gophercises/quiz/instructions.md
+++ gophercises/quiz/instructions.md
@@ -1,2 +1,3 @@
 1. Take argument form user and print it out to terminal
-
+2. Read Problems.csv file and print contents
+3. print each problem to user
--- gophercises/quiz/quiz.go
+++ gophercises/quiz/quiz.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
 
 	filePath := flag.String("filename", "random", "filepath obsolute path")
 	flag.Parse()
-	fmt.Println(*filePath)
+	//fmt.Println(*filePath)
+	f, err := os.Open(*filePath)
 
+	if err != nil {
+		panic(err)
+	}
+
+	r := csv.NewReader(f)
+
+	lines, err := r.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	for i := range lines {
+		fmt.Println("Problem: ", lines[i][0])
+		fmt.Println("answer: ", lines[i][1])
+	}
 }
